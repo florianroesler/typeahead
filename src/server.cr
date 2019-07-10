03ca225c4083f41256d0e74378d9e4764a46768f
Florian Roesler <florian.roesler@googlemail.com>
2019-07-10 20:44:48 +0200

    Add query capabilities to Kemal Server

--- src/server.cr
+++ src/server.cr
@@ -5,12 +5,25 @@ require "logger"
 Log = Logger.new(STDOUT)
 Log.level = Logger::DEBUG
 
-get "/" do
-	"I am alive. How are you?"
+require "./trie.cr"
+
+lines = File.read_lines("#{Dir.current}/data/words_alpha.txt")
+trie = Trie.new
+
+lines.each do |word|
+	trie.push(word.strip)
 end
 
-get "/find" do |env|
+before_all do |env|
+	env.response.content_type = "application/json"
+end
 
+get "/" do
+	{ status: "OK" }.to_json
+end
+
+get "/complete" do |env|
+	trie.complete(env.params.query.fetch("q", "")).to_json
 end
 
 Kemal.run
